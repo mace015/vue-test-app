a5911e3fd0ab3d05f17af5a0cc3ab41ed74a6dfe
Mace Muilman <mace@macemuilman.nl>
2016-11-22 22:46:48 +0100

    Simpele todo component toegevoegd.

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,8 +1,7 @@
 require('./bootstrap');
 
-
 Vue.component('example', require('./components/Example.vue'));
-
+Vue.component('todo', require('./components/Todo.vue'));
 
 const app = new Vue({
     el: '#app'
